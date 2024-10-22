299e8c2b62d2291c9ec985dbf11dd4d13b93541b
Dominik Modrzejewski <dominik.modrzejewski@wfirma.pl>
2024-10-22 20:41:59 +0200

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,33 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {CardComponent} from './card/card.component';
-import {BackgroundComponent} from './background/background.component';
-import {RestaurantsService} from './services/restaurants.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {CommonModule} from '@angular/common';
+import {Store} from "@ngrx/store";
+import { increment, decrement, reset } from '../store/progress.actions';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CardComponent, BackgroundComponent, HttpClientModule, CommonModule],
-  providers: [RestaurantsService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'foodApp';
+export class AppComponent implements OnInit{
+  progress$ = this.store.select('progress');
 
-  constructor(private restaurantsService: RestaurantsService) {
+  constructor(private store: Store<{ progress: number }>) {}
+
+  increment() {
+    this.store.dispatch(increment());
   }
-  ngOnInit() {
-    // this.restaurantsService.getRestaurants().subscribe((value) => {
-    //   console.log(value)
-    // })
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+
+  ngOnInit(): void {
+    this.store.select('progress').subscribe((val) => {
+      console.log(val)
+    })
   }
 }
