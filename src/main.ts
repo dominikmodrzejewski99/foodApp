8bfea511d72709af50cca97ab9519f0a1d5ece0b
dominikmodrzejewski99 <dominik.modrzejewski@wfirma.pl>
2025-04-04 20:15:38 +0200

    Added primeNG

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,3 @@ platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
-
-
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
-  ]
-};
