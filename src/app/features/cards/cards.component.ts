7154fa8fb6906290051730b306910ea1d06794c9
Dominik Modrzejewski <dominik.modrzejewski@wfirma.pl>
2025-04-07 09:59:04 +0200

    Added swagger for frontend

--- src/app/features/cards/cards.component.ts
+++ src/app/features/cards/cards.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Card} from "primeng/card";
+import { Card } from "primeng/card";
+import { httpResource } from "@angular/common/http";
+import { Caa0baf44bc64c9b94a44e9ed5bc418d200ResponseInner as Question } from "../../core/api/api";
+
+interface ErrorResponse {
+  message: string;
+}
 
 @Component({
   selector: 'app-cards',
@@ -10,5 +16,5 @@ import {Card} from "primeng/card";
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
-  // Twój kod
+  questionsResource = httpResource<Question[]>('http://127.0.0.1:8001/api/questions');
 }
